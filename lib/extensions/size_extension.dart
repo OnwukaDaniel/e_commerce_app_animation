73eda88d0413eee3416d59297e9336a7091e95ae
Daniel <onwukadaniel16@gmail.com>
2025-03-09 09:48:17 +0100

    Format

--- lib/extensions/size_extension.dart
+++ lib/extensions/size_extension.dart
@@ -4,6 +4,7 @@ extension ScreenSizes on int{
   SizedBox get h => SizedBox(height: toDouble());
   SizedBox get w => SizedBox(width: toDouble());
 }
+
 extension ScreenSizesDOuble on double{
   SizedBox get h => SizedBox(height: toDouble());
   SizedBox get w => SizedBox(width: toDouble());
