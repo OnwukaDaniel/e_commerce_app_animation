aee0460bf9f9af3100126b8461018020cb36993c
Daniel <onwukadaniel16@gmail.com>
2025-05-31 14:17:18 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce_app/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ECommerce App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Home(),
+    );
+  }
+}
